2616f55c4e13581b866b6d955519d43c262ee198
Anmol Khatri <anmol@192.168.100.109>
2022-07-31 11:46:36 +0530

    field level resolvers added

--- server.js
+++ server.js
@@ -1,18 +1,83 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express')
+const { ApolloServer, gql } = require('apollo-server-express')
 const dotEnv = require('dotenv');
 const cors = require('cors');
 const app = express();
 dotEnv.config();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+const { tasks, users } = require('./constants')
 
-app.use('/', (req, res, next) => {
-    res.send({greeting: 'Hi, express here'});
-})
 
-app.listen(PORT, () => {
-    console.log('Listening at port '+PORT);
-})
+
+const typeDefs = gql`
+    type Query {
+        greetings: String
+        tasks: [Task]
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        tasks: [Task!]
+    }
+
+    type Task {
+        id: ID!
+        name: String!
+        completed: Boolean!
+        user: User!
+    }
+`
+const resolvers = {
+    Query: {
+        greetings: () => "Hello",
+        tasks: () => tasks
+    },
+    Task: {
+        user: (parent) => {
+            console.log('parent =>', parent);
+           return users.find(user => {
+                console.log('user =>', user);
+                return user.id === parent.userId
+            })
+        }
+    }
+};
+
+async function startApolloServer (typeDefs, resolvers) {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+    await apolloServer.start()
+
+    // app.use(cors());
+    app.use(express.json());
+    
+    apolloServer.applyMiddleware({
+        app,
+        path: '/graphql'
+    })
+    const PORT = process.env.PORT || 3000;
+    
+    app.use('/', (req, res, next) => {
+        res.send({greeting: 'Hi, express here'});
+    });
+
+    await new Promise((req, res) => {
+        app.listen(PORT, () => {
+            console.log('Listening at port '+PORT);
+            console.log(`GraphQL endpoint: ${apolloServer.graphqlPath}`)
+        })
+
+    })
+}
+
+startApolloServer(typeDefs, resolvers);``
+
+
+
+
 
